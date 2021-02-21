120533bd48aba2955e136b2979ee1a265a340c5f
AlexandreRCS <77364739+AlexandreRCS@users.noreply.github.com>
2021-02-21 20:40:23 -0300

    Att-20-02-2021
    Adicionado a função de filtra paciente

--- js/busca.js
+++ js/busca.js
@@ -1,9 +1,22 @@
 var buscar = document.querySelector('#buscar-paciente');
 
 buscar.addEventListener('input',function(){
-     var pacientes = document.querySelectorAll('.paciente');
-     pacientes.forEach(function(paciente){
-        var nomes = paciente.querySelector(".info-nome").textContent
-        console.log(nomes)
-     })
+   var pacientes = document.querySelectorAll('.paciente');
+   var buscar = document.querySelector('#buscar-paciente');
+      if(buscar.value.length > 0){
+         pacientes.forEach(function(paciente){
+         var nomes = paciente.querySelector(".info-nome").textContent;
+         var esxpessao = new RegExp(buscar.value, 'i');
+         if(!esxpessao.test(nomes)){
+            paciente.classList.add('invisivel');
+         }else{
+            paciente.classList.remove('invisivel');
+         }
+       })
+      }else{
+         pacientes.forEach(function(paciente){
+            paciente.classList.remove('invisivel')
+         })
+    } 
+     
 })
